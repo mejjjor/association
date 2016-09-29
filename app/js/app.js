43ebd59fd5066e51705338f7a0156ccaf807fab2
mejjjor <mejjjor@github.com>
2016-09-29 15:54:57 +0200

    fix bug

--- app/js/app.js
+++ app/js/app.js
@@ -2,7 +2,6 @@ var riot = require('riot')
 var Backendless = require('backendless')
 var addAsso = require('./addAsso.tag')
 var addAsso = require('./showAssos.tag')
-var addAsso = require('./asso-item.tag')
 
 var APP_ID = "FFDC7C5B-9461-2230-FF25-390A64FE4400";
 var SECRET_KEY = "0DBD0BF2-7A45-1048-FFA7-B88507C8BF00";
